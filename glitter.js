536704c9bf22c46eeb25d325b35ca180ad439ff8
Kumar Abhirup <kumar@iqubex.com>
2018-05-02 11:30:00 +0530

    All packages added with Glitter.js

--- glitter.js
+++ glitter.js
@@ -1,1 +1,10 @@
-// First Commit is with Glitter.js
+console.log('Bot is starting...');
+
+var Twit = require('twit');
+var config = require('./config'); // Find config.js in same folder
+
+var T = new Twit(config);
+
+T.get('followers/list', {screen_name:'kumar_abhirup', count:10000}, function (err, data, response) {
+  console.log(data)
+})
