b2f7a46c276f3f90469ff8670a8ef5b6847f39c7
Kumar Abhirup <kumar@iqubex.com>
2018-05-02 22:25:35 +0530

    5000 follower ids are now retrieved and can be followed

--- glitter.js
+++ glitter.js
@@ -5,6 +5,19 @@ var config = require('./config'); // Find config.js in same folder
 
 var T = new Twit(config);
 
-T.get('followers/list', {screen_name:'kumar_abhirup', count:10000}, function (err, data, response) {
-  console.log(data)
-})
+// Pick a celebrity and grab all the Follower IDs
+T.get('followers/ids', { screen_name: 'elonmusk', count:'5000', cursor:'-1' },  function (err, data, response) {
+
+    for (var i = 0; i < 5000; i++) {
+
+        var peopleToFollow = data.ids[i];
+
+        // T.post('friendships/create', { id: peopleToFollow },  function (err, data, response) {
+        //   console.log(data);
+        // }); 
+
+        console.log(peopleToFollow);
+
+    }
+
+});
