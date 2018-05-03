9e45b405a875f682a53ee2ef0b58a706caae050b
Kumar Abhirup <kumar@iqubex.com>
2018-05-03 16:54:11 +0530

    The programme now follows the followers after every 5 seconds

--- glitter.js
+++ glitter.js
@@ -6,7 +6,7 @@ var config = require('./config'); // Find config.js in same folder
 var T = new Twit(config);
 
 // Pick a celebrity and grab all the Follower IDs
-T.get('followers/ids', { screen_name: 'elonmusk', count:'5000', cursor:'-1' },  function (err, data, response) {
+T.get('followers/ids', { screen_name: 'wesbos', count:'5', cursor:'-1' },  function (err, data, response) {
 
       // Functional Loop
       var i = 0;
@@ -25,7 +25,12 @@ T.get('followers/ids', { screen_name: 'elonmusk', count:'5000', cursor:'-1' },  
                     var screen_name = data.screen_name;
 
                     if(screen_name != null){ // Check if user is fake or not
-                      console.log(screen_name+" follows Elon Musk");
+
+                      // Follow the follower
+                      T.post('friendships/create', { screen_name: screen_name },  function (err, data, response) {
+                        console.log("Wesbos follower "+screen_name+" followed.")
+                      })
+
                     } else if (screen_name == null) {
                       return null;
                     }
@@ -39,59 +44,14 @@ T.get('followers/ids', { screen_name: 'elonmusk', count:'5000', cursor:'-1' },  
             i++;
 
             // How many times to loop
-            if(i < 5000) {
+            if(i < 10) {
                 timedLoop();
             }
 
-        }, 1); // After how many seconds. `1000` means 1 second.
+        }, 1000*5); // After how many seconds. `1000` means 1 second.
 
       }
-      timedLoop(); // Run the loop
 
-});
-
-
-// Pick a celebrity and grab all the Follower IDs
-T.get('followers/ids', { screen_name: 'imvKohli', count:'5000', cursor:'-1' },  function (err, data, response) {
-
-      // Functional Loop
-      var i = 0;
-      function timedLoop() { // Follow Function
-
-        setTimeout(function () { // Follows the user after every `x` seconds
-
-            /*=============================================>>>>>
-            = Thing to be done =
-            ===============================================>>>>>*/
-
-              // Convert id into screen_name
-                  var peopleToFollow = data.ids[i];
-                  T.get('users/show', { id: peopleToFollow },  function (err, data, response) {
-
-                    var screen_name = data.screen_name;
-
-                    if(screen_name != null){ // Check if user is fake or not
-                      console.log(screen_name+" follows Virat Kohli");
-                    } else if (screen_name == null) {
-                      return null;
-                    }
-
-                  });
-
-            /*= End of Thing to be done =*/
-            /*=============================================<<<<<*/
-
-            // Increase value of variable `i` by 1. (Increment)
-            i++;
-
-            // How many times to loop
-            if(i < 5000) {
-                timedLoop();
-            }
-
-        }, 1); // After how many seconds. `1000` means 1 second.
-
-      }
       timedLoop(); // Run the loop
 
 });
