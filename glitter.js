1c4c909ca4f417477255f1901827579df102e676
Kumar Abhirup <kumar@iqubex.com>
2018-05-03 15:15:03 +0530

    Check if the user id exists or not
    If the user ID doesn't exist, the id will get skipped.

--- glitter.js
+++ glitter.js
@@ -6,7 +6,7 @@ var config = require('./config'); // Find config.js in same folder
 var T = new Twit(config);
 
 // Pick a celebrity and grab all the Follower IDs
-T.get('followers/ids', { screen_name: 'elonmusk', count:'5000', cursor:'-1' },  function (err, data, response) {
+T.get('followers/ids', { screen_name: 'kumar_abhirup', count:'5000', cursor:'-1' },  function (err, data, response) {
 
       // Functional Loop
       var i = 0;
@@ -21,11 +21,16 @@ T.get('followers/ids', { screen_name: 'elonmusk', count:'5000', cursor:'-1' },  
               // Convert id into screen_name
                   var peopleToFollow = data.ids[i];
                   T.get('users/show', { id: peopleToFollow },  function (err, data, response) {
-                    var screen_name = data.screen_name;
-                    console.log(screen_name);
-                  });
 
-                  // console.log(peopleToFollow);
+                    var screen_name = data.screen_name;
+
+                    if(screen_name != null){ // Check if user is fake or not
+                      console.log(screen_name);
+                    } else if (screen_name == null) {
+                      return null;
+                    }
+
+                  });
 
             /*= End of Thing to be done =*/
             /*=============================================<<<<<*/
