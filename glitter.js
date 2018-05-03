1922aa20ef7b0f83d72130cb47041f723f6c87d4
Kumar Abhirup <kumar@iqubex.com>
2018-05-03 17:07:17 +0530

    Error now logs on to the console if any

--- glitter.js
+++ glitter.js
@@ -6,7 +6,7 @@ var config = require('./config'); // Find config.js in same folder
 var T = new Twit(config);
 
 // Pick a celebrity and grab all the Follower IDs
-T.get('followers/ids', { screen_name: 'wesbos', count:'5', cursor:'-1' },  function (err, data, response) {
+T.get('followers/ids', { screen_name: 'wesbos', count:'10', cursor:'-1' },  function (err, data, response) {
 
       // Functional Loop
       var i = 0;
@@ -26,10 +26,22 @@ T.get('followers/ids', { screen_name: 'wesbos', count:'5', cursor:'-1' },  funct
 
                     if(screen_name != null){ // Check if user is fake or not
 
-                      // Follow the follower
+                      // Follow and Unfollow the follower
                       T.post('friendships/create', { screen_name: screen_name },  function (err, data, response) {
-                        console.log("Wesbos follower "+screen_name+" followed.")
-                      })
+                        if(!err){
+                          console.log("Wesbos follower "+screen_name+" followed.")
+                        } else{
+                          console.log(err);
+                        }
+                      });
+
+                      /*T.post('friendships/destroy', { screen_name: screen_name },  function (err, data, response) {
+                        if(!err){
+                          console.log("Wesbos follower "+screen_name+" unfollowed.")
+                        } else{
+                          console.log(err);
+                        }
+                      });*/
 
                     } else if (screen_name == null) {
                       return null;
@@ -48,7 +60,7 @@ T.get('followers/ids', { screen_name: 'wesbos', count:'5', cursor:'-1' },  funct
                 timedLoop();
             }
 
-        }, 1000*5); // After how many seconds. `1000` means 1 second.
+        }, 5); // After how many seconds. `1000` means 1 second.
 
       }
 
