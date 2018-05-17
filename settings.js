2f6b88f7ece5f550d354e3096fa6f148eaa5e0e8
Kumar Abhirup <kumar@iqubex.com>
2018-05-17 23:39:43 +0530

    Changed the default PERSON_NAME to John Doe

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 // This file contains the screen_name of the person whose's followers to follow
 module.exports = Object.freeze({
-    PERSON_TWITTER_HANDLE: 'wesbos', // The Twitter handle (without @) of the person whose followers are to be followed
-    PERSON_NICKNAME: 'Wesbos' // A small name or a nickname of that person
+    PERSON_TWITTER_HANDLE: 'john_doe', // The Twitter handle (without @) of the person whose followers are to be followed
+    PERSON_NICKNAME: 'JohnDoe' // A small name or a nickname of that person
 });
